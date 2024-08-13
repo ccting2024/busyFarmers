7ba2e5ab0ccedf5a089a6a0aefe1db58d5d65fa0
Ting9798 <h821026888@gmail.com>
2024-08-13 16:50:42 +0800

    0813-1

--- busyJs.js
+++ busyJs.js
@@ -1,24 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let witheTopbar = document.querySelector('.witheTopbar');
     let greenTopbar = document.querySelector('.greenTopbar');
-    let overHero = document.getElementById('news'); // 確保這是你想要的元素
+    let overHero = document.getElementById('news');
 
-    if (!witheTopbar || !greenTopbar || !overHero) {
-        console.error('未能找到所需的元素');
-        return;
-    }
-
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         let overHeroTop = overHero.getBoundingClientRect().top + window.scrollY;
-
         console.log('scrollY:', window.scrollY, 'overHeroTop:', overHeroTop);
 
-        if (window.scrollY >= overHeroTop) {
+        if (this.window.scrollY == 0) {
             witheTopbar.classList.add('hidden');
             greenTopbar.classList.remove('hidden');
-        } else {
-            witheTopbar.classList.remove('hidden');
-            greenTopbar.classList.add('hidden');
         }
+
+        // if (window.scrollY >= overHeroTop) {
+
+        //     console.log('加入');
+        //     witheTopbar.classList.add('hidden');
+        //     greenTopbar.classList.remove('hidden');
+        // } else {
+
+        //     console.log('移出');
+        //     witheTopbar.classList.remove('hidden');
+        //     greenTopbar.classList.add('hidden');
+        // }
     });
 });
